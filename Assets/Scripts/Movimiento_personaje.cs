d34db04e1b7964c930121068dbb673ba646abe28
melissa992 <melyocoro@gmail.com>
2025-11-18 23:33:54 -0500

    Implementacion en el script de movimiento el salto del perosnaje y la verificacion cuando este tocando el suelo

--- Assets/Scripts/Movimiento_personaje.cs
+++ Assets/Scripts/Movimiento_personaje.cs
@@ -2,14 +2,14 @@ using UnityEngine;
 
 public class Movimiento_personaje : MonoBehaviour
 {
+    private Rigidbody2D rb2D;
+    private float move;
+    private bool isGrounded;
     public float velocidad = 2;
     public float jumpForce = 4;
     public Transform groundCheck;
     public float groundRadius = 0.1f;
     public LayerMask groundLayer;
-    private Rigidbody2D rb2D;
-    private float move;
-    private bool isGrounded;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -22,9 +22,21 @@ public class Movimiento_personaje : MonoBehaviour
     {
         move = Input.GetAxisRaw("Horizontal");
         rb2D.linearVelocity = new Vector2(move*velocidad, rb2D.linearVelocity.y);
+        
         if (move != 0)
         {
             transform.localScale = new Vector3(Mathf.Sign(move), 1, 1);
         }
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            rb2D.linearVelocity = new Vector2(rb2D.linearVelocity.x, jumpForce);
+        }
     }
+
+    private void FixedUpdate()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundRadius, groundLayer);
+    }
+    
 }
