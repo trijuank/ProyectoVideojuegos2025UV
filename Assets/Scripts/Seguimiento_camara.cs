0a59b5cb8846e5a3f4b8b73af2483d0b3d029a52
melissa992 <melyocoro@gmail.com>
2025-12-08 23:48:19 -0500

    Sincronizacion de escalas de fondo, tilemap y camara principal

--- Assets/Scripts/Seguimiento_camara.cs
+++ Assets/Scripts/Seguimiento_camara.cs
@@ -3,9 +3,13 @@ using UnityEngine;
 public class Seguimiento_camara : MonoBehaviour
 {
     public Transform target;
+    public float velocidadCamara = 0.125f;
+    public Vector3 desplazamiento;
     
     private void LateUpdate()
     {
-        transform.position = new Vector3(target.position.x, target.position.y, transform.position.z);
+        Vector3 posicionDeseada = target.position + desplazamiento;
+        Vector3 posicionSuavizada = Vector3.Lerp(transform.position, posicionDeseada, velocidadCamara);
+        transform.position = posicionSuavizada;
     }
 }
